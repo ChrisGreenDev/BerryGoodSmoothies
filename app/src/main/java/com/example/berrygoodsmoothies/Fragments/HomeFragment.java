ebb76351298801f4a391a1e8912cfc2d0e3bc318
Chris Green <w0795059@myscc.ca>
2022-11-06 21:53:13 -0500

    Added orange image to the top of all screens

--- app/src/main/java/com/example/berrygoodsmoothies/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/berrygoodsmoothies/Fragments/HomeFragment.java
@@ -3,10 +3,12 @@ package com.example.berrygoodsmoothies.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.berrygoodsmoothies.R;
 
@@ -61,6 +63,16 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button startButton = view.findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_nav_home_to_nav_smoothie);
+            }
+        });
+
+        return view;
     }
 }
