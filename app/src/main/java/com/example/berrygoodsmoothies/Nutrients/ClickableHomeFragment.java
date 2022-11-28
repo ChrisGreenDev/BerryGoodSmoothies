7fdfa37f2ca795a6ebe0e49694996b8c15c3ffed
Chris Green <w0795059@myscc.ca>
2022-11-27 21:49:16 -0500

    #37 Finished web intent

--- app/src/main/java/com/example/berrygoodsmoothies/Nutrients/ClickableHomeFragment.java
+++ app/src/main/java/com/example/berrygoodsmoothies/Nutrients/ClickableHomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.berrygoodsmoothies.Nutrients;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.berrygoodsmoothies.CustomMenuAdapters.CustomMenuAdapter;
 import com.example.berrygoodsmoothies.ClickableRecyclerView.SmoothieItem;
@@ -82,6 +85,14 @@ public class ClickableHomeFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
 
+        Button webBtn = view.findViewById(R.id.webButtonPotassium);
+        webBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://shape.com/healthy-eating/healthy-drinks/banana-smoothie-recipes"));
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
--- app/src/main/java/com/example/berrygoodsmoothies/Nutrients/VitaminAFragment.java
+++ app/src/main/java/com/example/berrygoodsmoothies/Nutrients/VitaminAFragment.java
@@ -1,5 +1,7 @@
 package com.example.berrygoodsmoothies.Nutrients;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.berrygoodsmoothies.ClickableRecyclerView.SmoothieItem;
 import com.example.berrygoodsmoothies.CustomMenuAdapters.CustomMenuAdapterVitaminA;
@@ -79,6 +82,14 @@ public class VitaminAFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
 
+        Button webBtn = view.findViewById(R.id.webButtonVA);
+        webBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.allrecipes.com/gallery/kale-smoothie-recipes/"));
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
