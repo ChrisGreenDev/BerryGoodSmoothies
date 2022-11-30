42b10e3ea72cc4cfe602827824f8d61c5a48a22e
Chris Green <w0795059@myscc.ca>
2022-11-29 19:41:12 -0500

    Settings working now

--- app/src/main/java/com/example/berrygoodsmoothies/MainActivity.java
+++ app/src/main/java/com/example/berrygoodsmoothies/MainActivity.java
@@ -2,12 +2,8 @@ package com.example.berrygoodsmoothies;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.Menu;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
@@ -51,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_home, R.id.nav_smoothie, R.id.nav_credits, R.id.nav_view_smoothies)
                 .setOpenableLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
